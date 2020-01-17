d32619b3d9fc543d5263ef255b3b52c476574d29
ariielm <arielhenrique3@gmail.com>
2020-01-16 22:29:54 -0300

    Checking nginx configurations

--- src/main/java/com/arielmolina/tasksapi/endpoint/HelloEndpoint.java
+++ src/main/java/com/arielmolina/tasksapi/endpoint/HelloEndpoint.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloEndpoint {
 
-    @GetMapping
+    @GetMapping("/api")
     public String welcome() {
         return "<h1>Welcome!</h1>";
     }
